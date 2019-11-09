eb52d7066ca21f0d0c06b3145ff7347f4dde01a9
Brad Coffield <bcoffield@gmail.com>
2019-11-09 08:50:26 -0700

    refactored fixed? I remain skeptical but... seems fine now

--- newbooks-refactored.js
+++ newbooks-refactored.js
@@ -50,28 +50,32 @@ let newBooksDynamism2 = (function(howManyWeWant) {
       //This is where we actually specify how many random numbers we want generated. This is likely different than the number of books we want to display. We need at least a few more than we want displayed because sometimes there isn't a book cover and that item won't be displayed.
       var ourRandoms = getRandomNumbers(50, jsonResponseLength);
       // console.log(ourRandoms);
-      console.log(theResults);
+      // console.log(theResults);
       return { theResults, ourRandoms };
       //   bookCoverGrab(result, ourRandoms); //call a function with the full results from the API call and our random numbers.
     })
     .then(results => {
-      //   console.log(results.ourRandoms, results.theResults);
+      // console.log(results.ourRandoms, results.theResults);
       let baseDom = document.getElementById("new-books-div");
       baseDom.insertAdjacentHTML("beforeend", "<ul id='new-books'>hi</ul>");
       let nextDom = document.getElementById("new-books");
       //   nextDom.style.display = "none";
-let i=0;
+      let i = 0;
       for (i; totalDisplayed < howManyWeWant; i++) {
-        let theIsbn =
-          results.theResults.docs[results.ourRandoms[i]].pnx.search.isbn[0];
-        let theTitle =
-          results.theResults.docs[results.ourRandoms[i]].pnx.display.title;
-        let theCatalogLink = `<a href="https://rocky-primo.hosted.exlibrisgroup.com/permalink/f/1j18u99/${results.theResults.docs[results.ourRandoms[i]].pnx.control.sourceid}${results.theResults.docs[results.ourRandoms[i]].pnx.control.sourcerecordid}"
+        // console.log(results.theResults.docs[results.ourRandoms[i]].pnx.search);
+        if (results.theResults.docs[results.ourRandoms[i]].pnx.search.isbn[0]) {
+          let theIsbn =
+            results.theResults.docs[results.ourRandoms[i]].pnx.search.isbn[0];
+          let theTitle =
+            results.theResults.docs[results.ourRandoms[i]].pnx.display.title;
+          let theCatalogLink = `<a href="https://rocky-primo.hosted.exlibrisgroup.com/permalink/f/1j18u99/${results.theResults.docs[results.ourRandoms[i]].pnx.control.sourceid}${results.theResults.docs[results.ourRandoms[i]].pnx.control.sourcerecordid}"
                  target="_blank">`;
 
-        let syndetics = `https://syndetics.com/index.aspx?isbn=${theIsbn}/MC.JPG&client=primo`;
+          let syndetics = `https://syndetics.com/index.aspx?isbn=${theIsbn}/MC.JPG&client=primo`;
 
-        addToDom(syndetics, theTitle, theCatalogLink, i);
+          addToDom(syndetics, theTitle, theCatalogLink, i);
+        }
+        else console.log('snatch!');
       }
     });
   function addToDom(theIMG, theTitle, catalogLink, i) {
