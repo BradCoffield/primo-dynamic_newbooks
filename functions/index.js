32bd4a5396c8fcf0c7401e287aa2356903eea00e
Brad Coffield <bcoffield@gmail.com>
2019-11-09 10:34:27 -0700

    works to get primo and res.send but doesn't go to firestore too......

--- functions/index.js
+++ functions/index.js
@@ -9,26 +9,33 @@
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
-
+const fetch = require("node-fetch");
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 admin.initializeApp();
-
-exports.primoBooksGrab = functions.https.onRequest((req, res) => {
-  const whatCollection = req.query.text;
+const db = admin.firestore();
+exports.primoNewBooksGrab = functions.https.onRequest((req, res) => {
+  
   let url =
-    "https://api-na.hosted.exlibrisgroup.com/primo/v1/search?q=lsr03%2Cexact%2Cnewbooks&vid=01TRAILS_ROCKY&tab=default_tab&limit=150&scope=P-01TRAILS_ROCKY&apikey=l8xx79d281ecc1e44f9f8b456a23c8cb1f47";
+  "https://api-na.hosted.exlibrisgroup.com/primo/v1/search?q=lsr03%2Cexact%2Cnewbooks&vid=01TRAILS_ROCKY&tab=default_tab&limit=150&scope=P-01TRAILS_ROCKY&apikey=l8xx79d281ecc1e44f9f8b456a23c8cb1f47";
 
-  fetch(url)
-    .then(resp => resp.json())
-    .then(function(result) {
-      let theResults = result;
-      let jsonContents = result.docs;
-      
-    
-   
-        const writeThings = admin.firestore().collection(whatCollection).add(jsonContents)
-        writeThings()
-        // res.json({result: `Message with ID: ${writeThings.id} added.`})
-    })
+fetch(url)
+  .then(resp => resp.json())
+  .then(function(result) {
+    let processedResults = [];
+    result.docs.forEach(i => {
+      if (i.pnx.search.isbn) {
+        processedResults.push({
+          isbn: i.pnx.search.isbn,
+          title: i.pnx.display.title,
+
+        });
+      }
+    });
+    db
+    .collection("primo-searches")
+    .doc("new-books")
+    .set({results: processedResults}, { merge: false });
+    res.send(processedResults);
+  });
 });
